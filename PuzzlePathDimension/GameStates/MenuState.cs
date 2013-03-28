0c410c160d361e889da99f5fbf58683aaa23f936
Michael Sandoval <sandovalmichael48@yahoo.com>
2013-03-28 15:10:11 -0700

    Uh, we got stuck.

---
+++ PuzzlePathDimension/GameStates/MenuState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PuzzlePathDimension.GameStates
+{
+    class MenuState : GameState
+    {
+      public void Update() {
+
+      }
+
+      public void Draw() {
+
+      }
+    }
+}
