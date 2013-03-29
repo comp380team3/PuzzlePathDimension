fd80baaa5fe90486c8f814e261a1efa6a80bd3a3
Jonathan Castello <twisolar@gmail.com>
2013-03-28 19:39:05 -0700

    Implement a LevelLoader and a barebones Level.

---
+++ PuzzlePathDimension/Models/Level.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PuzzlePathDimension {
+  class Level {
+    public Goal Goal { get; set; }
+    public Launcher Launcher { get; set; }
+    public List<Platform> Platforms { get; set; }
+  }
+}
