22e25b987674a8311a1403e258837fd9c043e66f
Jorenz <jorenz.paragas.393@my.csun.edu>
2013-03-31 13:31:14 -0700

    Create the PuzzlePathUnitTests project.
    The LauncherTest class contains an example of an actual unit test as
    well as one that intentionally fails.

---
+++ PuzzlePathUnitTests/LauncherTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using NUnit.Framework;
+using PuzzlePathDimension;
+
+namespace PuzzlePathUnitTests {
+  /// <summary>
+  /// The LauncherTest class tests the functionality of the launcher.
+  /// </summary>
+  [TestFixture]
+  public class LauncherTest {
+    /// <summary>
+    /// The starting angle of the launcher.
+    /// </summary>
+    private const float initialAngle = (float)Math.PI / 4;
+
+    /// <summary>
+    /// This test checks if the launcher's angle is adjusted properly.
+    /// </summary>
+    [Test]
+    public void MoveRightTest() {
+      const float moveOffset = (float) Math.PI / 64;
+
+      // Create the launcher.
+      Launcher l = new Launcher();
+      // We don't need a texture, so pass in null for the texture parameter.
+      l.Initialize(null, new Vector2(0, 0));
+      // Give the launcher a ball; otherwise, it won't move.
+      Ball b = new Ball();
+      l.LoadBall(b);
+
+      l.AdjustAngle(moveOffset);
+
+      // The last parameter is the tolerance since precision problems
+      // tend to come up with floating point numbers.
+      Assert.AreEqual(l.Angle, initialAngle + moveOffset, (float) 0.00001);
+    }
+
+    /// <summary>
+    /// Demonstration of a test that fails.
+    /// </summary>
+    [Test]
+    public void FailingTestExample() {
+      Assert.AreEqual(1, 2); // lol
+    }
+  }
+}
