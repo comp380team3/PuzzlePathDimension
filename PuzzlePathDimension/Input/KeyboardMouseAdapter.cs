25ae6721348f59b72289acb547252fa166ac75aa
Jorenz <jorenz.paragas.393@my.csun.edu>
2013-04-01 21:28:11 -0700

    Change the VirtualAdapter class to be an interface instead.

--- PuzzlePathDimension/Input/KeyboardMouseAdapter.cs
+++ PuzzlePathDimension/Input/KeyboardMouseAdapter.cs
@@ -17,7 +17,7 @@ namespace PuzzlePathDimension {
     /// Defines the keyboard input that corresponds to the Confirm input.
     /// </summary>
     /// <returns>The state of the keyboard input that is mapped to the Confirm input.</returns>
-    public override VirtualButtonState Confirm() {
+    public VirtualButtonState Confirm() {
       return Keyboard.GetState().IsKeyDown(Keys.Enter) || Keyboard.GetState().IsKeyDown(Keys.Space) ?
         VirtualButtonState.Pressed : VirtualButtonState.Released;
     }
@@ -26,7 +26,7 @@ namespace PuzzlePathDimension {
     /// Defines the keyboard input that corresponds to the Back input.
     /// </summary>
     /// <returns>The state of the keyboard input that is mapped to the Back input.</returns>
-    public override VirtualButtonState Back() {
+    public VirtualButtonState Back() {
       return Keyboard.GetState().IsKeyDown(Keys.Escape) ?
         VirtualButtonState.Pressed : VirtualButtonState.Released;
     }
@@ -35,7 +35,7 @@ namespace PuzzlePathDimension {
     /// Defines the keyboard input that corresponds to the Up input.
     /// </summary>
     /// <returns>The state of the keyboard input that is mapped to the Up input.</returns>
-    public override VirtualButtonState Up() {
+    public VirtualButtonState Up() {
       return Keyboard.GetState().IsKeyDown(Keys.Up) || Keyboard.GetState().IsKeyDown(Keys.W) ?
         VirtualButtonState.Pressed : VirtualButtonState.Released;
     }
@@ -44,7 +44,7 @@ namespace PuzzlePathDimension {
     /// Defines the keyboard input that corresponds to the Down input.
     /// </summary>
     /// <returns>The state of the keyboard input that is mapped to the Down input.</returns>
-    public override VirtualButtonState Down() {
+    public VirtualButtonState Down() {
       return Keyboard.GetState().IsKeyDown(Keys.Down) || Keyboard.GetState().IsKeyDown(Keys.S) ?
         VirtualButtonState.Pressed : VirtualButtonState.Released;
     }
@@ -53,7 +53,7 @@ namespace PuzzlePathDimension {
     /// Defines the keyboard input that corresponds to the Left input.
     /// </summary>
     /// <returns>The state of the keyboard input that is mapped to the Left input.</returns>
-    public override VirtualButtonState Left() {
+    public VirtualButtonState Left() {
       return Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.A) ?
         VirtualButtonState.Pressed : VirtualButtonState.Released;
     }
@@ -62,7 +62,7 @@ namespace PuzzlePathDimension {
     /// Defines the keyboard input that corresponds to the Right input.
     /// </summary>
     /// <returns>The state of the keyboard input that is mapped to the Right input.</returns>
-    public override VirtualButtonState Right() {
+    public VirtualButtonState Right() {
       return Keyboard.GetState().IsKeyDown(Keys.Right) || Keyboard.GetState().IsKeyDown(Keys.D) ?
         VirtualButtonState.Pressed : VirtualButtonState.Released;
     }
@@ -71,7 +71,7 @@ namespace PuzzlePathDimension {
     /// Defines the mouse input that corresponds to the Point input.
     /// </summary>
     /// <returns>The coordinates of the mouse pointer.</returns>
-    public override Point Point() {
+    public Point Point() {
       return new Point(Mouse.GetState().X, Mouse.GetState().Y);
     }
   }
--- PuzzlePathDimension/Input/VirtualAdapter.cs
+++ PuzzlePathDimension/Input/VirtualAdapter.cs
@@ -6,45 +6,45 @@ using Microsoft.Xna.Framework;
 
 namespace PuzzlePathDimension {
   /// <summary>
-  /// The VirtualAdapter class and its derived types map the input state from an input device
-  /// to input data that the VirtualController understands.
+  /// The VirtualAdapter interface and any classes that implement it map the input state 
+  /// from an input device to input data that the VirtualController understands.
   /// </summary>
-  public abstract class VirtualAdapter {
+  public interface VirtualAdapter {
     /// <summary>
     /// The equivalent of the Confirm input is defined here.
     /// </summary>
     /// <returns>Whether the input device's Confirm input is pressed or released.</returns>
-    public abstract VirtualButtonState Confirm();
+    VirtualButtonState Confirm();
     /// <summary>
     /// The equivalent of the Back input is defined here.
     /// </summary>
     /// <returns>Whether the input device's Back input is pressed or released.</returns>
-    public abstract VirtualButtonState Back();
+    VirtualButtonState Back();
     /// <summary>
     /// The equivalent of the Up input is defined here.
     /// </summary>
     /// <returns>Whether the input device's Up input is pressed or released.</returns>
-    public abstract VirtualButtonState Up();
+    VirtualButtonState Up();
     /// <summary>
     /// The equivalent of the Down input is defined here.
     /// </summary>
     /// <returns>Whether the input device's Down input is pressed or released.</returns>
-    public abstract VirtualButtonState Down();
+    VirtualButtonState Down();
     /// <summary>
     /// The equivalent of the Left input is defined here.
     /// </summary>
     /// <returns>Whether the input device's Left input is pressed or released.</returns>
-    public abstract VirtualButtonState Left();
+    VirtualButtonState Left();
     /// <summary>
     /// The equivalent of the Right input is defined here.
     /// </summary>
     /// <returns>Whether the input device's Right input is pressed or released.</returns>
-    public abstract VirtualButtonState Right();
+    VirtualButtonState Right();
 
     /// <summary>
     /// The equivalent of the Point input is defined here.
     /// </summary>
     /// <returns>The current position of the input device's Point input.</returns>
-    public abstract Point Point();
+    Point Point();
   }
 }
