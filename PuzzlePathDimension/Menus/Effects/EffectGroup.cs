62f343888cb32324129e152d6c794214d03d6d4f
Jonathan Castello <twisolar@gmail.com>
2013-04-14 21:37:46 -0700

    Implement EffectGroup for combining multiple effects together.

---
+++ PuzzlePathDimension/Menus/Effects/EffectGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace PuzzlePathDimension {
+  /// <summary>
+  /// Combines multiple effects into a single effect.
+  /// </summary>
+  /// <typeparam name="T">The type of cursor that this effect can be applied to.</typeparam>
+  class EffectGroup<T> : IEffect<T> {
+    private IEffect<T>[] Effects { get; set; }
+    public EffectGroup(params IEffect<T>[] effects) {
+      Effects = effects;
+    }
+
+    public T ApplyTo(T cursor) {
+      foreach (IEffect<T> effect in Effects)
+        cursor = effect.ApplyTo(cursor);
+      return cursor;
+    }
+  }
+}
