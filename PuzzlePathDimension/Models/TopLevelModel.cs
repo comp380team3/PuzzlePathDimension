291b42d6648b1029470d888dbab62869cdb5e8df
Jonathan Castello <twisolar@gmail.com>
2013-04-26 14:19:08 -0700

    Make the TopLevelModel controller ~not~ a WritableVirtualController.

--- PuzzlePathDimension/Models/TopLevelModel.cs
+++ PuzzlePathDimension/Models/TopLevelModel.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 namespace PuzzlePathDimension {
   // Please, please, please ensure that this does not become a god object.
   public class TopLevelModel {
-    public WritableVirtualController Controller { get; set; }
+    public VirtualController Controller { get; set; }
     public UserPrefs Prefs { get; set; }
     public Game Game { get; set; }
     public Scene Scene { get; set; }
