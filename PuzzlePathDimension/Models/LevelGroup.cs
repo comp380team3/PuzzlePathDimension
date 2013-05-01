f365d7f52a1cfcf36c0cfd337bef73d58829acbd
Jorenz <jorenz.paragas.393@my.csun.edu>
2013-05-01 16:35:33 -0700

    Create LevelGroup.cs.

---
+++ PuzzlePathDimension/Models/LevelGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PuzzlePathDimension {
+  public class LevelEntry {
+    [XmlAttribute("path")]
+    public string Path { get; set; }
+    [XmlAttribute("id")]
+    public string Id { get; set; }
+  }
+
+  [XmlRoot("Levels")]
+  public class LevelGroup {
+    [XmlElement("Level")]
+    public List<LevelEntry> Entries { get; set; }
+  }
+}
